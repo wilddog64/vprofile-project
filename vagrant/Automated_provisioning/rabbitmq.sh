4a83f1c84ac67e3afe354a52e73635f8c4bb1eab
chengkai liang <chengkli@cisco.com>
2023-02-20 06:30:26 -0800

    refactor rabitmq.sh so we can install rabitmq-server to centos9

--- vagrant/Automated_provisioning/rabbitmq.sh
+++ vagrant/Automated_provisioning/rabbitmq.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
-sudo yum install epel-release -y
+
+# update for CentOS9 stream
+# cd /tmp/
+# wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
+
+# install and configure rabbitmq-server, and erlang yum repo
+# curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
+# curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | sudo bash
+#
+# # enable rabbitmq_rabitmq-server yum repo cache
+# sudo yum makecache -y --disablerepo='*' --enablerepo='rabbitmq_rabbitmq-server'
+
 sudo yum update -y
-sudo yum install wget -y
-cd /tmp/
-wget http://packages.erlang-solutions.com/erlang-solutions-2.0-1.noarch.rpm
-sudo rpm -Uvh erlang-solutions-2.0-1.noarch.rpm
-sudo yum -y install erlang socat
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | sudo bash
-sudo yum install rabbitmq-server -y
-sudo systemctl start rabbitmq-server
+
+# now we install a configuration to enable repo for rabbitmq-server
+sudo yum install -y centos-release-rabbitmq-38
+
+sudo yum install -y rabbitmq-mqserver
 sudo systemctl enable rabbitmq-server
-sudo systemctl status rabbitmq-server
-sudo sh -c 'echo "[{rabbit, [{loopback_users, []}]}]." > /etc/rabbitmq/rabbitmq.config'
-sudo rabbitmqctl add_user test test
-sudo rabbitmqctl set_user_tags test administrator
-sudo systemctl restart rabbitmq-server
+sudo systemctl start rabbitmq-server
